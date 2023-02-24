9ec20e2b01efa4e94533d2302e13e9e64fc4d777
viniFurukawa <vinicius.carvalho@opus-software.com.br>
2023-02-23 21:39:53 -0300

    its working, for now

--- src/lib/pages/test-page.svelte
+++ src/lib/pages/test-page.svelte
@@ -1,62 +1,60 @@
-<script lang="ts">
-    import PeerService from "../services/peer-service/peer.service";
-    export let uuid: string;
-    let peer = PeerService.peer;
+<script>
+    import { Peer } from "peerjs";
+    import MediaDeviceType from "../shared/media-devices/media-device-type";
+    var peer = new Peer();
+    let codeid = "";
+    let youid = "";
 
-    let videocurrent;
-    let videoEl;
-
+    let videoOther;
+    let videoMe;
+    // GET YOU ID
+    peer.on("open", (id) => youid = id);
     // HANDLE CONNECTTION
     peer.on("call", async (call) => {
         // open webcam
         await navigator.mediaDevices
-            .getUserMedia({
-                video: true,
-                audio: true,
-            })
+            .getUserMedia(MediaDeviceType.ALL_ENABLED)
             .then((stream) => {
                 call.answer(stream);
                 call.on("stream", renderYouwebcam);
-                videocurrent.srcObject = stream;
-                videocurrent.play();
-            })
-            .catch((err) => console.log("err msg" + err));
+                videoOther.srcObject = stream;
+                videoOther.play();
+            });
     });
     // RENDER YOU WEBCAM HERE
     let renderYouwebcam = (stream) => {
         console.log(stream);
-        videoEl.srcObject = stream;
-        videoEl.play();
+        videoMe.srcObject = stream;
+        videoMe.play();
     };
 </script>
 
 <div>
+    you id cam = {youid}
+    <br />
+    code : <input type="" bind:value={codeid} name="" />
     <!-- BUTTON CONNECT TO FRIEND -->
     <button
         on:click={async () => {
             // OPEN YOU WEBAM
             await navigator.mediaDevices
-                .getUserMedia({
-                    video: true,
-                    audio: true,
-                })
+                .getUserMedia(MediaDeviceType.ALL_ENABLED)
                 .then((stream) => {
-                    let call = peer.call(uuid, stream);
-                    videocurrent.srcObject = stream;
-                    videocurrent.play();
+                    let call = peer.call(codeid, stream);
+                    videoOther.srcObject = stream;
+                    videoOther.play();
                     call.on("stream", renderYouwebcam);
-                })
-                .catch((err) => console.log("have error " + err));
+                });
         }}
     >connect</button>
 
     <div>
         <!-- VIDEO YOU FRIEND TAG HTML -->
-        <video bind:this={videoEl} width="400" height="400" autoplay>
+        <video id="other" bind:this={videoMe} width="400" height="400" autoplay>
             <track kind="captions" src="" />
         </video>
         <!-- YOU FACE CAM HERE -->
-        <video bind:this={videocurrent} width="400" height="400" autoplay>
+        <video id="me" bind:this={videoOther} width="400" height="400" autoplay>
             <track kind="captions" src="" />
         </video>
     </div>
